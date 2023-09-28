f71544ccfa8cea6574175316d3780b4eb80d87e7
Nicholas Whitt <nick.whitt@protonmail.com>
2023-09-28 12:26:16 -0400

    pages, api

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+const { data } = await useFetch('/api/hello')
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">Index page</h1>
+  <pre>{{ data }}</pre>
+</template>

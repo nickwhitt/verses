d36b77f2030776e66111e83dd13fc6d2693bacac
Nick <nick.whitt@protonmail.com>
2023-09-29 10:35:16 -0400

    v-for components

---
+++ components/verse.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps({
+  number: Number,
+  lines: Array<string>
+})
+</script>
+
+<template>
+  <li :value="number">
+    <span v-for="line in lines">{{ line }}</span>
+  </li>
+</template>

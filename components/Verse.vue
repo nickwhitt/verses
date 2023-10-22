15c4f4a37b5b4356aaac132a8137aa6399b7fe82
Nicholas Whitt <nick.whitt@protonmail.com>
2023-10-22 11:32:37 -0400

    layout

---
+++ components/Verse.vue
@@ -0,0 +1,12 @@
+<template>
+  <figure class="p-2">
+    <figcaption class="inline">
+      <cite>{{ number }}</cite>
+    </figcaption>
+    <slot />
+  </figure>
+</template>
+
+<script setup>
+const props = defineProps(["number"]);
+</script>
--- pages/god-chooses-younger-twin.vue
+++ pages/god-chooses-younger-twin.vue
@@ -1,222 +1,228 @@
 <template>
-  <main class="lg:container mx-auto px-2 bg-white shadow-md">
+  <main class="lg:container mx-auto px-2 bg-white">
     <p class="text-center text-xl font-bold py-6">
       Genesis 25:19-34<br />Word for Word
     </p>
-    <div class="grid sm:grid-cols-3 gap-2 sm:text-sm lg:text-base xl:text-lg">
-      <article class="">
-        <p class="text-center pb-4">New King James Version</p>
-        <ol class="list-decimal list-inside">
-          <li value="19">
-            This is the genealogy of Isaac, Abraham's son. Abraham begot Isaac.
-          </li>
-          <li>
-            Isaac was forty years old when he took Rebekah as wife, the daughter
-            of Bethuel the Syrian of Padan Aram, the sister of Laban the Syrian.
-          </li>
-          <li>
-            Now Isaac pleaded with the LORD for his wife, because she was
-            barren; and the LORD granted his plea, and Rebekah his wife
-            conceived.
-          </li>
-          <li>
-            But the children struggled together within her; and she said, "If
-            all is well, why am I like this?" So she went to inquire of the
-            LORD.
-          </li>
-          <li>
-            And the LORD said to her: "Two nations are in your womb, Two peoples
-            shall be separated from your body; One people shall be stronger than
-            the other, And the older shall serve the younger."
-          </li>
-          <li>
-            So when her days were fulfilled for her to give birth, indeed there
-            were twins in her womb.
-          </li>
-          <li>
-            And the first came out red. He was like a hairy garment all over; so
-            they called his name Esau.
-          </li>
-          <li>
-            Afterward his brother came out, and his hand took hold of Esau's
-            heel; so his name was called Jacob. Isaac was sixty years old when
-            she bore them.
-          </li>
-          <li>
-            So the boys grew. And Esau was a skillful hunter, a man of the
-            field; but Jacob was a mild man, dwelling in tents.
-          </li>
-          <li>
-            And Isaac loved Esau because he ate of his game, but Rebekah loved
-            Jacob.
-          </li>
-          <li>
-            Now Jacob cooked a stew; and Esau came in from the field, and he was
-            weary.
-          </li>
-          <li>
-            And Esau said to Jacob, "Please feed me with that same red stew, for
-            I am weary." Therefore his name was called Edom.
-          </li>
-          <li>But Jacob said, "Sell me your birthright as of this day."</li>
-          <li>
-            And Esau said, "Look, I am about to die; so what is this birthright
-            to me?"
-          </li>
-          <li>
-            Then Jacob said, "Swear to me as of this day." So he swore to him,
-            and sold his birthright to Jacob.
-          </li>
-          <li>
-            And Jacob gave Esau bread and stew of lentils; then he ate and
-            drank, arose, and went his way. Thus Esau despised his birthright.
-          </li>
-        </ol>
-      </article>
-      <article class="">
-        <p class="text-center pb-4">King James Version</p>
-        <ol class="list-decimal list-inside">
-          <li value="19">
-            And these <em>are</em> the generations of Isaac, Abraham's son:
-            Abraham begat Isaac:
-          </li>
-          <li>
-            And Isaac was forty years old when he took Rebekah to wife, the
-            daughter of Bethuel the Syrian of Padanaram, the sister to Laban the
-            Syrian.
-          </li>
-          <li>
-            And Isaac intreated the LORD for his wife, because she
-            <em>was</em> barren: and the LORD was intreated of him, and Rebekah
-            his wife conceived.
-          </li>
-          <li>
-            And the children struggled together within her; and she said, If
-            <em>it be</em> so, why <em>am</em> I thus? And she went to enquire
-            of the LORD.
-          </li>
-          <li>
-            And the LORD said unto her, Two nations <em>are</em> in thy womb,
-            and two manner of people shall be separated from thy bowels; and
-            <em>the one</em> people shall be stronger than
-            <em>the other</em> people; and the elder shall serve the younger.
-          </li>
-          <li>
-            And when her days to be delivered were fulfilled, behold,
-            <em>there were</em> twins in her womb. Gen
-          </li>
-          <li>
-            And the first came out red, all over like an hairy garment; and they
-            called his name Esau.
-          </li>
-          <li>
-            And after that came his brother out, and his hand took hold on
-            Esau's heel; and his name was called Jacob: and Isaac
-            <em>was</em> threescore years old when she bare them.
-          </li>
-          <li>
-            And the boys grew: and Esau was a cunning hunter, a man of the
-            field; and Jacob <em>was</em> a plain man, dwelling in tents.
-          </li>
-          <li>
-            And Isaac loved Esau, because he did eat of <em>his</em> venison:
-            but Rebekah loved Jacob.
-          </li>
-          <li>
-            And Jacob sod pottage: and Esau came from the field, and he
-            <em>was</em> faint:
-          </li>
-          <li>
-            And Esau said to Jacob, Feed me, I pray thee, with that same red
-            <em>pottage</em>; for I <em>am</em> faint: therefore was his name
-            called Edom.
-          </li>
-          <li>And Jacob said, Sell me this day thy birthright.</li>
-          <li>
-            And Esau said, Behold, I <em>am</em> at the point to die: and what
-            profit shall this birthright do to me?
-          </li>
-          <li>
-            And Jacob said, Swear to me this day; and he sware unto him: and he
-            sold his birthright unto Jacob.
-          </li>
-          <li>
-            Then Jacob gave Esau bread and pottage of lentiles; and he did eat
-            and drink, and rose up, and went his way: thus Esau despised
-            <em>his</em> birthright.
-          </li>
-        </ol>
-      </article>
+    <div class="grid sm:grid-cols-3 sm:text-sm lg:text-base xl:text-lg">
+      <p class="text-center pb-4">King James Version</p>
+      <p class="text-center pb-4">New King James Version</p>
+      <p class="text-center pb-4">English Standard Version</p>
 
-      <article class="">
-        <p class="text-center pb-4">English Standard Version</p>
-        <ol class="list-decimal list-inside">
-          <li value="19">
-            These are the generations of Isaac, Abraham's son: Abraham fathered
-            Isaac,
-          </li>
-          <li>
-            and Isaac was forty years old when he took Rebekah, the daughter of
-            Bethuel the Aramean of Paddan-aram, the sister of Laban the Aramean,
-            to be his wife.
-          </li>
-          <li>
-            And Isaac prayed to the LORD for his wife, because she was barren.
-            And the LORD granted his prayer, and Rebekah his wife conceived.
-          </li>
-          <li>
-            The children struggled together within her, and she said, "If it is
-            thus, why is this happening to me?" So she went to inquire of the
-            LORD.
-          </li>
-          <li>
-            And the LORD said to her, "Two nations are in your womb, and two
-            peoples from within you shall be divided; the one shall be stronger
-            than the other, the older shall serve the younger."
-          </li>
-          <li>
-            When her days to give birth were completed, behold, there were twins
-            in her womb.
-          </li>
-          <li>
-            The first came out red, all his body like a hairy cloak, so they
-            called his name Esau.
-          </li>
-          <li>
-            Afterward his brother came out with his hand holding Esau's heel, so
-            his name was called Jacob. Isaac was sixty years old when she bore
-            them.
-          </li>
-          <li>
-            When the boys grew up, Esau was a skillful hunter, a man of the
-            field, while Jacob was a quiet man, dwelling in tents.
-          </li>
-          <li>
-            Isaac loved Esau because he ate of his game, but Rebekah loved
-            Jacob.
-          </li>
-          <li>
-            Once when Jacob was cooking stew, Esau came in from the field, and
-            he was exhausted.
-          </li>
-          <li>
-            And Esau said to Jacob, "Let me eat some of that red stew, for I am
-            exhausted!" (Therefore his name was called Edom.)
-          </li>
-          <li>Jacob said, "Sell me your birthright now."</li>
-          <li>
-            Esau said, "I am about to die; of what use is a birthright to me?"
-          </li>
-          <li>
-            Jacob said, "Swear to me now." So he swore to him and sold his
-            birthright to Jacob.
-          </li>
-          <li>
-            Then Jacob gave Esau bread and lentil stew, and he ate and drank and
-            rose and went his way. Thus Esau despised his birthright.
-          </li>
-        </ol>
-      </article>
+      <Verse number="19" translation="KJV">
+        And these <i>are</i> the generations of Isaac, Abraham's son: Abraham
+        begat Isaac:
+      </Verse>
+      <Verse number="19" translation="NKJV">
+        <u>This <i>is</i></u> the <u>genealogy</u> of Isaac, Abraham's son.
+        Abraham begot Isaac.
+      </Verse>
+      <Verse number="19" translation="ESV">
+        These are the generations of Isaac, Abraham's son: Abraham
+        <u>fathered</u> Isaac,
+      </Verse>
+
+      <Verse number="20" translation="KJV">
+        And Isaac was forty years old when he took Rebekah to wife, the daughter
+        of Bethuel the Syrian of Padanaram, the sister to Laban the Syrian.
+      </Verse>
+      <Verse number="20" translation="NKJV">
+        Isaac was forty years old when he took Rebekah as wife, the daughter of
+        Bethuel the Syrian of Padan Aram, the sister of Laban the Syrian.
+      </Verse>
+      <Verse number="20" translation="ESV">
+        and Isaac was forty years old when he took Rebekah, the daughter of
+        Bethuel the Aramean of Paddan-aram, the sister of Laban the Aramean, to
+        be his wife.
+      </Verse>
+
+      <Verse number="21" translation="KJV">
+        And Isaac intreated the LORD for his wife, because she
+        <i>was</i> barren: and the LORD was intreated of him, and Rebekah his
+        wife conceived.
+      </Verse>
+      <Verse number="21" translation="NKJV">
+        Now Isaac pleaded with the LORD for his wife, because she
+        <i>was</i> barren; and the LORD granted his plea, and Rebekah his wife
+        conceived.
+      </Verse>
+      <Verse number="21" translation="ESV">
+        And Isaac prayed to the LORD for his wife, because she was barren. And
+        the LORD granted his prayer, and Rebekah his wife conceived.
+      </Verse>
+
+      <Verse number="22" translation="KJV">
+        And the children struggled together within her; and she said, If
+        <i>it be</i> so, why <i>am</i> I thus? And she went to enquire of the
+        LORD.
+      </Verse>
+      <Verse number="22" translation="NKJV">
+        But the children struggled together within her; and she said, "If [all
+        is] well, why <i>am I like</i> this?" So she went to inquire of the
+        LORD.
+      </Verse>
+      <Verse number="22" translation="ESV">
+        The children struggled together within her, and she said, "If it is
+        thus, why is this happening to me?" So she went to inquire of the LORD.
+      </Verse>
+
+      <Verse number="23" translation="KJV">
+        And the LORD said unto her, Two nations <i>are</i> in thy womb, and two
+        manner of people shall be separated from thy bowels; and
+        <i>the one</i> people shall be stronger than <i>the other</i> people;
+        and the elder shall serve the younger.
+      </Verse>
+      <Verse number="23" translation="NKJV">
+        And the LORD said to her: "Two nations <i>are</i> in your womb, Two
+        peoples shall be separated from your body; <i>One</i> people shall be
+        stronger than the other, And the older shall serve the younger."
+      </Verse>
+      <Verse number="23" translation="ESV">
+        And the LORD said to her, "Two nations are in your womb, and two peoples
+        from within you shall be divided; the one shall be stronger than the
+        other, the older shall serve the younger."
+      </Verse>
+
+      <Verse number="24" translation="KJV">
+        And when her days to be delivered were fulfilled, behold,
+        <i>there were</i> twins in her womb.
+      </Verse>
+      <Verse number="24" translation="NKJV">
+        So when her days were fulfilled <i>for her</i> to give birth, indeed
+        [there were] twins in her womb.
+      </Verse>
+      <Verse number="24" translation="ESV">
+        When her days to give birth were completed, behold, there were twins in
+        her womb.
+      </Verse>
+
+      <Verse number="25" translation="KJV">
+        And the first came out red, all over like an hairy garment; and they
+        called his name Esau.
+      </Verse>
+      <Verse number="25" translation="NKJV">
+        And the first came out red. <i>He was</i> like a hairy garment all over;
+        so they called his name Esau.
+      </Verse>
+      <Verse number="25" translation="ESV">
+        The first came out red, all his body like a hairy cloak, so they called
+        his name Esau.
+      </Verse>
+
+      <Verse number="26" translation="KJV">
+        And after that came his brother out, and his hand took hold on Esau's
+        heel; and his name was called Jacob: and Isaac <i>was</i> threescore
+        years old when she bare them.
+      </Verse>
+      <Verse number="26" translation="NKJV">
+        Afterward his brother came out, and his hand took hold of Esau's heel;
+        so his name was called Jacob. Isaac <i>was</i> sixty years old when she
+        bore them.
+      </Verse>
+      <Verse number="26" translation="ESV">
+        Afterward his brother came out with his hand holding Esau's heel, so his
+        name was called Jacob. Isaac was sixty years old when she bore them.
+      </Verse>
+
+      <Verse number="27" translation="KJV">
+        And the boys grew: and Esau was a cunning hunter, a man of the field;
+        and Jacob <i>was</i> a plain man, dwelling in tents.
+      </Verse>
+      <Verse number="27" translation="NKJV">
+        So the boys grew. And Esau was a skillful hunter, a man of the field;
+        but Jacob was a mild man, dwelling in tents.
+      </Verse>
+      <Verse number="27" translation="ESV">
+        When the boys grew up, Esau was a skillful hunter, a man of the field,
+        while Jacob was a quiet man, dwelling in tents.
+      </Verse>
+
+      <Verse number="28" translation="KJV">
+        And Isaac loved Esau, because he did eat of <i>his</i> venison: but
+        Rebekah loved Jacob.
+      </Verse>
+      <Verse number="28" translation="NKJV">
+        And Isaac loved Esau because he ate <i>of his</i> game, but Rebekah
+        loved Jacob.
+      </Verse>
+      <Verse number="28" translation="ESV">
+        Isaac loved Esau because he ate of his game, but Rebekah loved Jacob.
+      </Verse>
+
+      <Verse number="29" translation="KJV">
+        And Jacob sod pottage: and Esau came from the field, and he
+        <i>was</i> faint:
+      </Verse>
+      <Verse number="29" translation="NKJV">
+        Now Jacob cooked a stew; and Esau came in from the field, and he
+        <i>was</i> weary.
+      </Verse>
+      <Verse number="29" translation="ESV">
+        Once when Jacob was cooking stew, Esau came in from the field, and he
+        was exhausted.
+      </Verse>
+
+      <Verse number="30" translation="KJV">
+        And Esau said to Jacob, Feed me, I pray thee, with that same red
+        <i>pottage</i>; for I <i>am</i> faint: therefore was his name called
+        Edom.
+      </Verse>
+      <Verse number="30" translation="NKJV">
+        And Esau said to Jacob, "Please feed me with that same red <i>stew</i>,
+        for I <i>am</i> weary." Therefore his name was called Edom.
+      </Verse>
+      <Verse number="30" translation="ESV">
+        And Esau said to Jacob, "Let me eat some of that red stew, for I am
+        exhausted!" (Therefore his name was called Edom.)
+      </Verse>
+
+      <Verse number="31" translation="KJV">
+        And Jacob said, Sell me this day thy birthright.
+      </Verse>
+      <Verse number="31" translation="NKJV">
+        But Jacob said, "Sell me your birthright as of this day."
+      </Verse>
+      <Verse number="31" translation="ESV">
+        Jacob said, "Sell me your birthright now."
+      </Verse>
+
+      <Verse number="32" translation="KJV">
+        And Esau said, Behold, I <i>am</i> at the point to die: and what profit
+        shall this birthright do to me?
+      </Verse>
+      <Verse number="32" translation="NKJV">
+        And Esau said, "Look, I <i>am</i> about to die; so what <i>is</i> this
+        birthright to me?"
+      </Verse>
+      <Verse number="32" translation="ESV">
+        Esau said, "I am about to die; of what use is a birthright to me?"
+      </Verse>
+
+      <Verse number="33" translation="KJV">
+        And Jacob said, Swear to me this day; and he sware unto him: and he sold
+        his birthright unto Jacob.
+      </Verse>
+      <Verse number="33" translation="NKJV">
+        Then Jacob said, "Swear to me as of this day." So he swore to him, and
+        sold his birthright to Jacob.
+      </Verse>
+      <Verse number="33" translation="ESV">
+        Jacob said, "Swear to me now." So he swore to him and sold his
+        birthright to Jacob.
+      </Verse>
+
+      <Verse number="34" translation="KJV">
+        Then Jacob gave Esau bread and pottage of lentiles; and he did eat and
+        drink, and rose up, and went his way: thus Esau despised <i>his</i>
+        birthright.
+      </Verse>
+      <Verse number="34" translation="NKJV">
+        And Jacob gave Esau bread and stew of lentils; then he ate and drank,
+        arose, and went his way. Thus Esau despised <i>his</i> birthright.
+      </Verse>
+      <Verse number="34" translation="ESV">
+        Then Jacob gave Esau bread and lentil stew, and he ate and drank and
+        rose and went his way. Thus Esau despised his birthright.
+      </Verse>
     </div>
   </main>
 </template>

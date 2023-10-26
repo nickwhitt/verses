93217d424d1b4943dc58d0f59e8209a0df7bfe25
Nicholas Whitt <nick.whitt@protonmail.com>
2023-10-26 14:00:19 -0400

    layout

--- pages/jacob-called-israel.vue
+++ pages/jacob-called-israel.vue
@@ -1,37 +1,229 @@
 <template>
+  <Body class="text-slate-700" />
   <div class="xl:container mx-auto px-4">
     <hgroup class="py-12">
-      <p>God Calls Abraham's Family</p>
-      <h1 class="text-2xl font-bold text-zinc-900">Jacob Called Israel</h1>
-      <p class="flex items-center gap-2">
+      <p class="text-slate-500">God Calls Abraham's Family</p>
+      <h1 class="text-2xl font-bold">Jacob Called Israel</h1>
+      <p class="flex items-center text-slate-500 gap-2">
         <BookOpenIcon class="h-5 w-5" />
         Genesis 32:22-32
       </p>
     </hgroup>
-    <main class="grid sm:grid-cols-2 gap-4">
-      <article class="prose">
-        <h2 class="not-prose sm:text-center">King James Version</h2>
+    <main class="grid sm:grid-cols-2 gap-4 py-4">
+      <article class="">
+        <h2 class="sm:text-center font-semibold mb-4">King James Version</h2>
+        <div class="sm:text-sm md:text-base xl:text-lg">
+          <p class="indent-6 leading-7">
+            22And he rose up that night, and took his two wives, and his two
+            womenservants, and his eleven sons, and passed over the ford Jabbok.
+            23And he took them, and sent them over the brook, and sent over that
+            he had. 24And Jacob was left alone; and there wrestled a man with
+            him until the breaking of the day. 25And when he saw that he
+            prevailed not against him, he touched the hollow of his thigh; and
+            the hollow of Jacob's thigh was out of joint, as he wrestled with
+            him. 26And he said, Let me go, for the day breaketh. And he said, I
+            will not let thee go, except thou bless me.
+          </p>
+          <p class="indent-6 leading-7">
+            27And he said unto him, What <i>is</i> thy name? And he said, Jacob.
+          </p>
+          <p class="indent-6 leading-7">
+            28And he said, Thy name shall be called no more Jacob, but Israel:
+            for as a prince hast thou power with God and with men, and hast
+            prevailed.
+          </p>
+          <p class="indent-6 leading-7">
+            29And Jacob asked <i>him</i>, and said, Tell <i>me</i>, I pray thee,
+            thy name. And he said, Wherefore <i>is</i> it <i>that</i> thou dost
+            ask after my name? And he blessed him there.
+          </p>
+          <p class="indent-6 leading-7">
+            30And Jacob called the name of the place Peniel: for I have seen God
+            face to face, and my life is preserved. 31And as he passed over
+            Penuel the sun rose upon him, and he halted upon his thigh.
+            32Therefore the children of Israel eat not <i>of</i> the sinew which
+            shrank, which <i>is</i> upon the hollow of the thigh, unto this day:
+            because he touched the hollow of Jacob's thigh in the sinew that
+            shrank.
+          </p>
+        </div>
       </article>
-      <article class="prose">
-        <h2 class="not-prose sm:text-center">New International Version</h2>
+      <article class="">
+        <h2 class="sm:text-center font-semibold mb-4">
+          New International Version
+        </h2>
+        <div class="sm:text-sm md:text-base xl:text-lg">
+          <p class="indent-6 leading-7">
+            22That night Jacob got up and took his two wives, his two female
+            servants and his eleven sons and crossed the ford of the Jabbok.
+            23After he had sent them across the stream, he sent over all his
+            possessions. 24So Jacob was left alone, and a man wrestled with him
+            till daybreak. 25When the man saw that he could not overpower him,
+            he touched the socket of Jacob's hip so that his hip was wrenched as
+            he wrestled with the man. 26Then the man said, "Let me go, for it is
+            daybreak."
+          </p>
+          <p class="indent-6 leading-7">
+            But Jacob replied, "I will not let you go unless you bless me."
+          </p>
+          <p class="indent-6 leading-7">
+            27The man asked him, "What is your name?"
+          </p>
+          <p class="indent-6 leading-7">"Jacob," he answered.</p>
+          <p class="indent-6 leading-7">
+            28Then the man said, "Your name will no longer be Jacob, but Israel,
+            because you have struggled with God and with humans and have
+            overcome."
+          </p>
+          <p class="indent-6 leading-7">
+            29Jacob said, "Please tell me your name."
+          </p>
+          <p class="indent-6 leading-7">
+            But he replied, "Why do you ask my name?" Then he blessed him there.
+          </p>
+          <p class="indent-6 leading-7">
+            30So Jacob called the place Peniel, saying, "It is because I saw God
+            face to face, and yet my life was spared."
+          </p>
+          <p class="indent-6 leading-7">
+            31The sun rose above him as he passed Peniel, and he was limping
+            because of his hip. 32Therefore to this day the Israelites do not
+            eat the tendon attached to the socket of the hip, because the socket
+            of Jacob's hip was touched near the tendon.
+          </p>
+        </div>
       </article>
     </main>
-    <aside class="hidden md:grid grid-cols-3 lg:grid-cols-5 py-4 gap-4">
-      <div class="contents prose">
-        <h2 class="hidden lg:grid not-prose text-center">
-          New King James Version
-        </h2>
-        <h2 class="not-prose text-center">English Standard Version</h2>
-        <h2 class="not-prose text-center">Christian Standard Bible</h2>
-        <h2 class="hidden lg:grid not-prose text-center">
-          New English Translation
-        </h2>
-        <h2 class="not-prose text-center">New Living Translation</h2>
+    <aside class="hidden md:grid grid-cols-3 lg:grid-cols-5 gap-2 py-4">
+      <div class="contents font-medium text-center">
+        <h2 class="hidden lg:block">New King James Version</h2>
+        <h2 class="">English Standard Version</h2>
+        <h2 class="">Christian Standard Bible</h2>
+        <h2 class="hidden lg:block">New English Translation</h2>
+        <h2 class="">New Living Translation</h2>
+      </div>
+
+      <div class="contents text-sm xl:text-base">
+        <p class="hidden lg:block">
+          22And he arose that night and took his two wives, his two female
+          servants, and his eleven sons, and crossed over the ford of Jabbok.
+        </p>
+        <p class="">
+          22The same night he arose and took his two wives, his two female
+          servants, and his eleven children, and crossed the ford of the Jabbok.
+        </p>
+        <p class="">
+          22During the night Jacob got up and took his two wives, his two slave
+          women, and his eleven sons, and crossed the ford of Jabbok.
+        </p>
+        <p class="hidden lg:block">
+          22During the night Jacob quickly took his two wives, his two female
+          servants, and his eleven sons and crossed the ford of the Jabbok.
+        </p>
+        <p class="">
+          22During the night Jacob got up and took his two wives, his two
+          servant wives, and his eleven sons and crossed the Jabbok River with
+          them.
+        </p>
+
+        <p class="hidden lg:block">
+          23He took them, sent them over the brook, and sent over what he had.
+        </p>
+        <p>
+          23He took them and sent them across the stream, and everything else
+          that he had.
+        </p>
+        <p></p>
+        <p class="hidden lg:block"></p>
+        <p></p>
+
+        <p class="hidden lg:block">
+          24Then Jacob was left alone; and a Man wrestled with him until the
+          breaking of day.
+        </p>
+        <p></p>
+        <p></p>
+        <p class="hidden lg:block"></p>
+        <p></p>
+
+        <p class="hidden lg:block">
+          25Now when He saw that He did not prevail against him, He touched the
+          socket of his hip; and the socket of Jacob's hip was out of joint as
+          He wrestled with him.
+        </p>
+        <p></p>
+        <p></p>
+        <p class="hidden lg:block"></p>
+        <p></p>
+
+        <p class="hidden lg:block">
+          26And He said, "Let Me go, for the day breaks." But he said, "I will
+          not let You go unless You bless me!"
+        </p>
+        <p></p>
+        <p></p>
+        <p class="hidden lg:block"></p>
+        <p></p>
+
+        <p class="hidden lg:block">
+          27So He said to him, "What <i>is</i> your name?" He said, "Jacob."
+        </p>
+        <p></p>
+        <p></p>
+        <p class="hidden lg:block"></p>
+        <p></p>
+
+        <p class="hidden lg:block">
+          28And He said, "Your name shall no longer be called Jacob, but Israel;
+          for you have struggled with God and with men, and have prevailed."
+        </p>
+        <p></p>
+        <p></p>
+        <p class="hidden lg:block"></p>
+        <p></p>
+
+        <p class="hidden lg:block">
+          29Then Jacob asked, saying, "Tell <i>me</i> Your name, I pray." And He
+          said, "Why <i>is</i> it <i>that</i> you ask about My name?" And He
+          blessed him there.
+        </p>
+        <p></p>
+        <p></p>
+        <p class="hidden lg:block"></p>
+        <p></p>
+
+        <p class="hidden lg:block">
+          30So Jacob called the name of the place Peniel: "For I have seen God
+          face to face, and my life is preserved."
+        </p>
+        <p></p>
+        <p></p>
+        <p class="hidden lg:block"></p>
+        <p></p>
+
+        <p class="hidden lg:block">
+          31Just as he crossed over Penuel the sun rose on him, and he limped on
+          his hip.
+        </p>
+        <p></p>
+        <p></p>
+        <p class="hidden lg:block"></p>
+        <p></p>
+
+        <p class="hidden lg:block">
+          32Therefore to this day the children of Israel do not eat the muscle
+          that shrank, which <i>is</i> on the hip socket, because He touched the
+          socket of Jacob's hip in the muscle that shrank.
+        </p>
+        <p></p>
+        <p></p>
+        <p class="hidden lg:block"></p>
+        <p></p>
       </div>
     </aside>
   </div>
 </template>
 
 <script setup>
-import { BookOpenIcon, ChevronRightIcon } from "@heroicons/vue/24/outline";
+import { BookOpenIcon } from "@heroicons/vue/24/outline";
 </script>

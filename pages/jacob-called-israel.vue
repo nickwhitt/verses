d9f5a4da9a44c33b2e9ba6eec9b2025bad874f78
Nicholas Whitt <nick.whitt@protonmail.com>
2023-10-28 22:23:45 -0400

    serve

--- pages/jacob-called-israel.vue
+++ pages/jacob-called-israel.vue
@@ -1,3 +1,13 @@
+<script setup>
+import { BookOpenIcon } from "@heroicons/vue/24/outline";
+useSeoMeta({
+  title: "Jacob Called Israel",
+  description: "Genesis 32:22-32",
+});
+
+const { data: gen32 } = await useFetch("/api/gen32");
+</script>
+
 <template>
   <Body class="text-slate-700 dark:text-slate-300 dark:bg-slate-900" />
   <div class="xl:container mx-auto px-4">
@@ -14,7 +24,7 @@
       <article class="prose prose-slate dark:prose-invert">
         <h2 class="sm:text-center">King James Version</h2>
         <div class="sm:prose-sm md:prose-base xl:prose-lg">
-          <p class="">
+          <p>
             <b>22</b>And he rose up that night, and took his two wives, and his
             two womenservants, and his eleven sons, and passed over the ford
             Jabbok.
@@ -117,264 +127,17 @@
         <h2>New Living Translation</h2>
       </div>
 
-      <ul class="contents text-sm xl:text-base">
-        <li data-reference="GEN 32:22 NKJV" class="hidden lg:block">
-          <b>22</b>And he arose that night and took his two wives, his two
-          female servants, and his eleven sons, and crossed over the ford of
-          Jabbok.
-        </li>
-        <li data-reference="GEN 32:22 ESV">
-          <b>22</b>The same night he arose and took his two wives, his two
-          female servants, and his eleven children, and crossed the ford of the
-          Jabbok.
-        </li>
-        <li data-reference="GEN 32:22 CSB">
-          <b>22</b>During the night Jacob got up and took his two wives, his two
-          slave women, and his eleven sons, and crossed the ford of Jabbok.
-        </li>
-        <li data-reference="GEN 32:22 NET" class="hidden lg:block">
-          <b>22</b>During the night Jacob quickly took his two wives, his two
-          female servants, and his eleven sons and crossed the ford of the
-          Jabbok.
-        </li>
-        <li data-reference="GEN 32:22 NLT">
-          <b>22</b>During the night Jacob got up and took his two wives, his two
-          servant wives, and his eleven sons and crossed the Jabbok River with
-          them.
-        </li>
-
-        <li data-reference="GEN 32:23 NKJV" class="hidden lg:block">
-          <b>23</b>He took them, sent them over the brook, and sent over what he
-          had.
-        </li>
-        <li data-reference="GEN 32:23 ESV">
-          <b>23</b>He took them and sent them across the stream, and everything
-          else that he had.
-        </li>
-        <li data-reference="GEN 32:23 CSB">
-          <b>23</b>He took them and sent them across the stream, along with all
-          his possessions.
-        </li>
-        <li data-reference="GEN 32:23 NET" class="hidden lg:block">
-          <b>23</b>He took them and sent them across the stream along with all
-          his possessions.
-        </li>
-        <li data-reference="GEN 32:23 NLT">
-          <b>23</b>After taking them to the other side, he sent over all his
-          possessions.
-        </li>
-
-        <li data-reference="GEN 32:24 NKJV" class="hidden lg:block">
-          <b>24</b>Then Jacob was left alone; and a Man wrestled with him until
-          the breaking of day.
-        </li>
-        <li data-reference="GEN 32:24 ESV">
-          <b>24</b>And Jacob was left alone. And a man wrestled with him until
-          the breaking of the day.
-        </li>
-        <li data-reference="GEN 32:24 CSB">
-          <b>24</b>Jacob was left alone, and a man wrestled with him until
-          daybreak.
-        </li>
-        <li data-reference="GEN 32:24 NET" class="hidden lg:block">
-          <b>24</b>So Jacob was left alone. Then a man wrestled with him until
-          daybreak.
-        </li>
-        <li data-reference="GEN 32:24 NLT">
-          <b>24</b>This left Jacob all alone in the camp, and a man came and
-          wrestled with him until the dawn began to break.
-        </li>
-
-        <li data-reference="GEN 32:25 NKJV" class="hidden lg:block">
-          <b>25</b>Now when He saw that He did not prevail against him, He
-          touched the socket of his hip; and the socket of Jacob's hip was out
-          of joint as He wrestled with him.
-        </li>
-        <li data-reference="GEN 32:25 ESV">
-          <b>25</b>When the man saw that he did not prevail against Jacob, he
-          touched his hip socket, and Jacob's hip was put out of joint as he
-          wrestled with him.
-        </li>
-        <li data-reference="GEN 32:25 CSB">
-          <b>25</b>When the man saw that he could not defeat him, he struck
-          Jacob's hip socket as they wrestled and dislocated his hip.
-        </li>
-        <li data-reference="GEN 32:25 NET" class="hidden lg:block">
-          <b>25</b>When the man saw that he could not defeat Jacob, he struck
-          the socket of his hip so the socket of Jacob's hip was dislocated
-          while he wrestled with him.
-        </li>
-        <li data-reference="GEN 32:25 NLT">
-          <b>25</b>When the man saw that he would not win the match, he touched
-          Jacob's hip and wrenched it out of its socket.
-        </li>
-
-        <li data-reference="GEN 32:26 NKJV" class="hidden lg:block">
-          <b>26</b>And He said, "Let Me go, for the day breaks." But he said, "I
-          will not let You go unless You bless me!"
-        </li>
-        <li data-reference="GEN 32:26 ESV">
-          <b>26</b>Then he said, "Let me go, for the day has broken." But Jacob
-          said, "I will not let you go unless you bless me."
-        </li>
-        <li data-reference="GEN 32:26 CSB">
-          <b>26</b>Then he said to Jacob, "Let me go, for it is daybreak." But
-          Jacob said, "I will not let you go unless you bless me."
-        </li>
-        <li data-reference="GEN 32:26 NET" class="hidden lg:block">
-          <b>26</b>Then the man said, "Let me go, for the dawn is breaking." "I
-          will not let you go," Jacob replied, "unless you bless me."
-        </li>
-        <li data-reference="GEN 32:26 NLT">
-          <b>26</b>Then the man said, "Let me go, for the dawn is breaking!" But
-          Jacob said, "I will not let you go unless you bless me."
-        </li>
-
-        <li data-reference="GEN 32:27 NKJV" class="hidden lg:block">
-          <b>27</b>So He said to him, "What <i>is</i> your name?" He said,
-          "Jacob."
-        </li>
-        <li data-reference="GEN 32:27 ESV">
-          <b>27</b>And he said to him, "What is your name?" And he said,
-          "Jacob."
-        </li>
-        <li data-reference="GEN 32:27 CSB">
-          <b>27</b>"What is your name? " the man asked. "Jacob," he replied.
-        </li>
-        <li data-reference="GEN 32:27 NET" class="hidden lg:block">
-          <b>27</b>The man asked him, "What is your name?" He answered, "Jacob."
-        </li>
-        <li data-reference="GEN 32:27 NLT">
-          <b>27</b>"What is your name?" the man asked. He replied, "Jacob."
-        </li>
-
-        <li data-reference="GEN 32:28 NKJV" class="hidden lg:block">
-          <b>28</b>And He said, "Your name shall no longer be called Jacob, but
-          Israel; for you have struggled with God and with men, and have
-          prevailed."
-        </li>
-        <li data-reference="GEN 32:28 ESV">
-          <b>28</b>Then he said, "Your name shall no longer be called Jacob, but
-          Israel, for you have striven with God and with men, and have
-          prevailed."
-        </li>
-        <li data-reference="GEN 32:28 CSB">
-          <b>28</b>"Your name will no longer be Jacob," he said. "It will be
-          Israel because you have struggled with God and with men and have
-          prevailed."
-        </li>
-        <li data-reference="GEN 32:28 NET" class="hidden lg:block">
-          <b>28</b>"No longer will your name be Jacob," the man told him, "but
-          Israel, because you have fought with God and with men and have
-          prevailed."
-        </li>
-        <li data-reference="GEN 32:28 NLT">
-          <b>28</b>"Your name will no longer be Jacob," the man told him. "From
-          now on you will be called Israel, because you have fought with God and
-          with men and have won."
-        </li>
-
-        <li data-reference="GEN 32:29 NKJV" class="hidden lg:block">
-          <b>29</b>Then Jacob asked, saying, "Tell <i>me</i> Your name, I pray."
-          And He said, "Why <i>is</i> it <i>that</i> you ask about My name?" And
-          He blessed him there.
-        </li>
-        <li data-reference="GEN 32:29 ESV">
-          <b>29</b>Then Jacob asked him, "Please tell me your name." But he
-          said, "Why is it that you ask my name?" And there he blessed him.
-        </li>
-        <li data-reference="GEN 32:29 CSB">
-          <b>29</b>Then Jacob asked him, "Please tell me your name." But he
-          answered, "Why do you ask my name? " And he blessed him there.
-        </li>
-        <li data-reference="GEN 32:29 NET" class="hidden lg:block">
-          <b>29</b>Then Jacob asked, "Please tell me your name." "Why do you ask
-          my name?" the man replied. Then he blessed Jacob there.
-        </li>
-        <li data-reference="GEN 32:29 NLT">
-          <b>29</b>"Please tell me your name," Jacob said. "Why do you want to
-          know my name?" the man replied. Then he blessed Jacob there.
-        </li>
-
-        <li data-reference="GEN 32:30 NKJV" class="hidden lg:block">
-          <b>30</b>So Jacob called the name of the place Peniel: "For I have
-          seen God face to face, and my life is preserved."
-        </li>
-        <li data-reference="GEN 32:30 ESV">
-          <b>30</b>So Jacob called the name of the place Peniel, saying, "For I
-          have seen God face to face, and yet my life has been delivered."
-        </li>
-        <li data-reference="GEN 32:30 CSB">
-          <b>30</b>Jacob then named the place Peniel, "For I have seen God face
-          to face," he said, "yet my life has been spared."
-        </li>
-        <li data-reference="GEN 32:30 NET" class="hidden lg:block">
-          <b>30</b>So Jacob named the place Peniel, explaining, "Certainly I
-          have seen God face to face and have survived."
-        </li>
-        <li data-reference="GEN 32:30 NLT">
-          <b>30</b>Jacob named the place Peniel (which means "face of God"), for
-          he said, "I have seen God face to face, yet my life has been spared."
-        </li>
-
-        <li data-reference="GEN 32:31 NKJV" class="hidden lg:block">
-          <b>31</b>Just as he crossed over Penuel the sun rose on him, and he
-          limped on his hip.
-        </li>
-        <li data-reference="GEN 32:31 ESV">
-          <b>31</b>The sun rose upon him as he passed Penuel, limping because of
-          his hip.
-        </li>
-        <li data-reference="GEN 32:31 CSB">
-          <b>31</b>The sun shone on him as he passed by Penuel -- limping
-          because of his hip.
-        </li>
-        <li data-reference="GEN 32:31 NET" class="hidden lg:block">
-          <b>31</b>The sun rose over him as he crossed over Penuel, but he was
-          limping because of his hip.
-        </li>
-        <li data-reference="GEN 32:31 NLT">
-          <b>31</b>The sun was rising as Jacob left Peniel, and he was limping
-          because of the injury to his hip.
-        </li>
-
-        <li data-reference="GEN 32:32 NKJV" class="hidden lg:block">
-          <b>32</b>Therefore to this day the children of Israel do not eat the
-          muscle that shrank, which <i>is</i> on the hip socket, because He
-          touched the socket of Jacob's hip in the muscle that shrank.
-        </li>
-        <li data-reference="GEN 32:32 ESV">
-          <b>32</b>Therefore to this day the people of Israel do not eat the
-          sinew of the thigh that is on the hip socket, because he touched the
-          socket of Jacob's hip on the sinew of the thigh.
-        </li>
-        <li data-reference="GEN 32:32 CSB">
-          <b>32</b>That is why, still today, the Israelites don't eat the thigh
-          muscle that is at the hip socket: because he struck Jacob's hip socket
-          at the thigh muscle.
-        </li>
-        <li data-reference="GEN 32:32 NET" class="hidden lg:block">
-          <b>32</b>That is why to this day the Israelites do not eat the sinew
-          which is attached to the socket of the hip, because he struck the
-          socket of Jacob's hip near the attached sinew.
-        </li>
-        <li data-reference="GEN 32:32 NLT">
-          <b>32</b>(Even today the people of Israel don't eat the tendon near
-          the hip socket because of what happened that night when the man
-          strained the tendon of Jacob's hip.)
-        </li>
+      <ul v-for="verse in gen32" class="contents text-sm xl:text-base">
+        <li
+          v-for="t in ['nkjv', 'esv', 'csb', 'net', 'nlt']"
+          :data-reference="'GEN 32:' + verse.number + ' ' + t.toUpperCase()"
+          :class="{ 'hidden lg:block': t == 'nkjv' || t == 'net' }"
+          v-html="verse[t]"
+        ></li>
       </ul>
     </aside>
   </div>
 </template>
-
-<script setup>
-import { BookOpenIcon } from "@heroicons/vue/24/outline";
-useSeoMeta({
-  title: "Jacob Called Israel",
-  description: "Genesis 32:22-32",
-});
-</script>
 
 <style>
 i {

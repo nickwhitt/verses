e9b239b0febee5b609546603383bd476f8a2a032
Nicholas Whitt <nick.whitt@protonmail.com>
2023-11-03 14:20:19 -0400

    list

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,39 @@
+<script setup>
+const units = [
+  {
+    name: "God Call's Abraham's Family",
+    lessons: [
+      { name: "The Call of Abram", slug: "call-of-abram" },
+      {
+        name: "God Chooses The Younger Twin",
+        slug: "god-chooses-younger-twin",
+      },
+      { name: "Jacob Called Israel", slug: "jacob-called-israel" },
+      { name: "The Scepter Given to Judah", slug: "scepter-given-to-judah" },
+    ],
+  },
+  {
+    name: "Out of Slavery to Nationhood",
+    lessons: [
+      { name: "The Birth of Moses" },
+      { name: "Song of Moses" },
+      { name: "The Call of Gideon" },
+      { name: "Who Is King?" },
+      { name: "David Anointed as King" },
+    ],
+  },
+  {
+    name: "We Are God's Artwork",
+    lessons: [
+      { name: "God Picked You!" },
+      { name: "Christ Is Wisdom" },
+      { name: "We Are God's Handiwork" },
+      { name: "God Gives Tools for Our Protection" },
+    ],
+  },
+];
+</script>
+
 <template>
   <Body class="text-slate-700 dark:text-slate-300 dark:bg-slate-900" />
   <div class="xl:container mx-auto px-4 xl:max-w-screen-xl">
@@ -5,65 +41,29 @@
       <h1 class="m-0">International Sunday School Lessons</h1>
     </header>
 
+    <div class="prose prose-slate dark:prose-invert">
+      <h2 class="mb-4 md:mb-6">God's Exceptional Choice</h2>
+    </div>
+
     <div class="grid md:grid-cols-3 gap-4">
-      <div class="md:border dark:border-slate-700 rounded-lg h-fit">
-        <h3 class="py-2 md:px-4 border-b dark:border-slate-700">
-          God Callls Abraham's Family
-        </h3>
+      <div
+        v-for="unit in units"
+        class="md:border dark:border-slate-700 rounded-lg h-fit"
+      >
+        <div class="py-2 md:px-4 border-b dark:border-slate-700">
+          <h3 class="text-lg tracking-tight font-medium">
+            {{ unit.name }}
+          </h3>
+        </div>
         <ol class="py-2 px-4 list-none space-y-2">
-          <li>
+          <li v-for="lesson in unit.lessons">
             <NuxtLink
-              to="call-of-abram"
+              :to="lesson.slug"
               class="not-prose hover:dark:text-slate-200 hover:underline"
             >
-              The Call of Abram
+              {{ lesson.name }}
             </NuxtLink>
           </li>
-          <li>
-            <NuxtLink
-              to="god-chooses-younger-twin"
-              class="not-prose hover:dark:text-slate-200 hover:underline"
-            >
-              God Chooses The Younger Twin
-            </NuxtLink>
-          </li>
-          <li>
-            <NuxtLink
-              to="jacob-called-israel"
-              class="not-prose hover:dark:text-slate-200 hover:underline"
-            >
-              Jacob Called Israel
-            </NuxtLink>
-          </li>
-          <li>
-            <NuxtLink
-              to="scepter-given-to-judah"
-              class="not-prose hover:dark:text-slate-200 hover:underline"
-            >
-              The Scepter Given to Judah
-            </NuxtLink>
-          </li>
-        </ol>
-      </div>
-
-      <div class="border rounded-lg h-fit">
-        <h3 class="p-4 border-b">Out of Slavery to Nationhood</h3>
-        <ol class="p-4 list-none space-y-2">
-          <li>The Birth of Moses</li>
-          <li>Song of Moses</li>
-          <li>The Call of Gideon</li>
-          <li>Who Is King?</li>
-          <li>David Anointed as King</li>
-        </ol>
-      </div>
-
-      <div class="border rounded-lg h-fit">
-        <h3 class="p-4 border-b">We Are God's Artwork</h3>
-        <ol class="p-4 list-none space-y-2">
-          <li>God Picked You!</li>
-          <li>Christ Is Wisdom</li>
-          <li>We Are God's Handiwork</li>
-          <li>God Gives Tools for Our Protection</li>
         </ol>
       </div>
     </div>

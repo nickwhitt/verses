77bcc174c7b3d647ee67c03961acd734f08adf40
Nick <nick.whitt@protonmail.com>
2023-11-07 11:21:35 -0500

    theme

--- app.vue
+++ app.vue
@@ -3,8 +3,17 @@
 
   <nav class="bg-gray-800">
     <div class="xl:container mx-auto xl:max-w-screen-xl px-4">
-      <div class="flex h-16 items-center">
+      <div class="flex h-16 items-center justify-between">
         <NuxtLink to="/" class="text-white">Vs</NuxtLink>
+        <button
+          class="hidden dark:flex"
+          @click="$colorMode.preference = 'light'"
+        >
+          <SunIcon class="h-6 w-6 text-gray-200" />
+        </button>
+        <button class="dark:hidden" @click="$colorMode.preference = 'dark'">
+          <MoonIcon class="h-6 w-6 text-gray-200" />
+        </button>
       </div>
     </div>
   </nav>
@@ -15,6 +24,7 @@
 </template>
 
 <script setup lang="ts">
+import { SunIcon, MoonIcon } from "@heroicons/vue/24/outline";
 useHead({
   htmlAttrs: { lang: "en-US" },
   link: [

ed357cbc128de1ac22af64d744895de5ecd4ab14
Nick Whitt <nick.whitt@protonmail.com>
2025-05-03 13:26:45 -0400

    books layout

---
+++ app/pages/books/job/2.vue
@@ -0,0 +1,78 @@
+<template>
+  <UContainer>
+    <h1 class="text-2xl italic font-bold">Job</h1>
+
+    <div class="grid md:grid-cols-2 gap-x-4 lg:gap-x-6">
+      <section class="my-6">
+        <h2>New King James Version (NKJV)</h2>
+
+        <h3 class="text-secondary font-semibold">Satan Attacks Job's Health</h3>
+        <p class="text-pretty">
+          <span class="float-left text-4xl text-secondary font-bold me-1">2</span>
+          <span class="text-xs font-extralight align-text-top me-0.5">1</span>Again there was a day when the sons of God came to present themselves before the <Lord />, and Satan came also among them to present himself before the <Lord />.
+          <span class="text-xs font-extralight align-text-top me-0.5">2</span>And the <Lord /> said to Satan, "From where do you come?"
+        </p>
+        <p class="indent-6 text-pretty">
+          Satan answered the <Lord /> and said, "From going to and fro on the earth, and from walking back and forth on it."
+        </p>
+        <p class="indent-6 text-pretty">
+          <span class="text-xs font-extralight align-text-top me-0.5">3</span>Then the <Lord /> said to Satan, "Have you considered My servant Job, that <em>there is</em> none like him on the earth, a blameless and upright man, one who fears God and shuns evil? And still he holds fast to his integrity, although you incited Me against him, to destroy him without cause."
+        </p>
+        <p class="indent-6 text-pretty">
+          <span class="text-xs font-extralight align-text-top me-0.5">4</span>So Satan answered the <Lord /> and said, "Skin for skin! Yes, all that a man has he will give for his life.
+          <span class="text-xs font-extralight align-text-top me-0.5">5</span>But stretch out Your hand now, and touch his bone and his flesh, and he will surely curse You to Your face!"
+        </p>
+        <p class="indent-6 text-pretty">
+          <span class="text-xs font-extralight align-text-top me-0.5">6</span>And the <Lord /> said to Satan, "Behold, he <em>is</em> in your hand, but spare his life."
+        </p>
+        <p class="indent-6 text-pretty">
+          <span class="text-xs font-extralight align-text-top me-0.5">7</span>So Satan went out from the presence of the <Lord />, and struck Job with painful boils from the sole of his foot to the crown of his head.
+          <span class="text-xs font-extralight align-text-top me-0.5">8</span>And he took for himself a potsherd with which to scrape himself while he sat in the midst of the ashes.
+        </p>
+        <p class="indent-6 text-pretty">
+          <span class="text-xs font-extralight align-text-top me-0.5">9</span>Then his wife said to him, "Do you still hold fast to your integrity? Curse God and die!"
+        </p>
+        <p class="indent-6 text-pretty">
+          <span class="text-xs font-extralight align-text-top me-0.5">10</span>But he said to her, "You speak as one of the foolish women speaks. Shall we indeed accept good from God, and shall we not accept adversity?" In all this Job did not sin with his lips.
+        </p>
+
+        <h3 class="mt-4 text-secondary font-semibold">Job's Three Friends</h3>
+        <p class="indent-6 text-pretty">
+          <span class="text-xs font-extralight align-text-top me-0.5">11</span>Now when Job's three friends heard of all this adversity that had come upon him, each one came from his own place&mdash;Eliphaz the Temanite, Bildad the Shuhite, and Zophar the Naamathite. For they had made an appointment together to come and mourn with him, and to comfort him.
+          <span class="text-xs font-extralight align-text-top me-0.5">12</span>And when they raised their eyes from afar, and did not recognize him, they lifted their voices and wept; and each one tore his robe and sprinkled dust on his head toward heaven.
+          <span class="text-xs font-extralight align-text-top me-0.5">13</span>So they sat down with him on the ground seven days and seven nights, and no one spoke a word to him, for they saw that <em>his</em> grief was very great.
+        </p>
+      </section>
+
+      <section class="my-6">
+        <h2>English Standard Version (ESV)</h2>
+
+        <h3 class="text-secondary font-semibold">Satan Attacks Job's Health</h3>
+        <p class="text-pretty">
+          <span class="float-left text-4xl text-secondary font-bold me-1">2</span>
+          <span class="text-xs font-extralight align-text-top me-0.5">1</span>Again there was a day when the sons of God came to present themselves before the <Lord />, and Satan also came among them to present himself before the <Lord />.
+          <span class="text-xs font-extralight align-text-top me-0.5">2</span>And the <Lord /> said to Satan, "From where have you come?" Satan answered the <Lord /> and said, "From going to and fro on the earth, and from walking up and down on it."
+          <span class="text-xs font-extralight align-text-top me-0.5">3</span>And the <Lord /> said to Satan, "Have you considered my servant Job, that there is none like him on the earth, a blameless and upright man, who fears God and turns away from evil? He still holds fast his integrity, although you incited me against him to destroy him without reason."
+          <span class="text-xs font-extralight align-text-top me-0.5">4</span>Then Satan answered the <Lord /> and said, "Skin for skin! All that a man has he will give for his life.
+          <span class="text-xs font-extralight align-text-top me-0.5">5</span>But stretch out your hand and touch his bone and his flesh, and he will curse you to your face."
+          <span class="text-xs font-extralight align-text-top me-0.5">6</span>And the <Lord /> said to Satan, "Behold, he is in your hand; only spare his life."
+        </p>
+        <p class="indent-6 text-pretty">
+          <span class="text-xs font-extralight align-text-top me-0.5">7</span>So Satan went out from the presence of the <Lord /> and struck Job with loathsome sores from the sole of his foot to the crown of his head.
+          <span class="text-xs font-extralight align-text-top me-0.5">8</span>And he took a piece of broken pottery with which to scrape himself while he sat in the ashes.
+        </p>
+        <p class="indent-6 text-pretty">
+          <span class="text-xs font-extralight align-text-top me-0.5">9</span>Then his wife said to him, "Do you still hold fast your integrity? Curse God and die."
+          <span class="text-xs font-extralight align-text-top me-0.5">10</span>But he said to her, "You speak as one of the foolish women would speak. Shall we receive good from God, and shall we not receive evil?" In all this Job did not sin with his lips.
+        </p>
+
+        <h3 class="mt-4 text-secondary font-semibold">Job's Three Friends</h3>
+        <p class="indent-6 text-pretty">
+          <span class="text-xs font-extralight align-text-top me-0.5">11</span>Now when Job's three friends heard of all this evil that had come upon him, they came each from his own place, Eliphaz the Temanite, Bildad the Shuhite, and Zophar the Naamathite. They made an appointment together to come to show him sympathy and comfort him.
+          <span class="text-xs font-extralight align-text-top me-0.5">12</span>And when they saw him from a distance, they did not recognize him. And they raised their voices and wept, and they tore their robes and sprinkled dust on their heads toward heaven.
+          <span class="text-xs font-extralight align-text-top me-0.5">13</span>And they sat with him on the ground seven days and seven nights, and no one spoke a word to him, for they saw that his suffering was very great.
+        </p>
+      </section>
+    </div>
+  </UContainer>
+</template>

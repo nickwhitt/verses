d16dcfc799c7701ae6a5c06802c4cc6d8c61a82f
Nick Whitt <nick.whitt@protonmail.com>
2025-05-11 10:49:00 -0400

    app default layout

---
+++ app/components/ColorModeButton.vue
@@ -0,0 +1,28 @@
+<script setup>
+const colorMode = useColorMode()
+
+const isDark = computed({
+  get() {
+    return colorMode.value === 'dark'
+  },
+  set(_isDark) {
+    colorMode.preference = _isDark ? 'dark' : 'light'
+  }
+})
+</script>
+
+<template>
+  <ClientOnly v-if="!colorMode?.forced">
+    <UButton
+      class="hover:text-white"
+      :icon="isDark ? 'i-lucide-moon' : 'i-lucide-sun'"
+      color="neutral"
+      variant="link"
+      @click="isDark = !isDark"
+    />
+
+    <template #fallback>
+      <div class="size-8" />
+    </template>
+  </ClientOnly>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,104 +1,13 @@
 <template>
-  <UContainer>
-    <h1 class="text-xl italic font-bold">Genesis</h1>
+  <UCard class="my-6">
+    <template #header>
+      <h2>header</h2>
+    </template>
 
-    <div class="grid md:grid-cols-2 gap-x-4 lg:gap-x-6">
-      <section class="my-6">
-        <h2>New King James Version (NKJV)</h2>
-        <h3 class="text-secondary font-semibold">God's Covenant with Abram</h3>
-        
-        <p class="text-pretty">
-          <span class="float-left text-4xl text-secondary font-bold me-1">15</span>
-          <span class="text-xs font-extralight align-text-top me-0.5">1</span>After these things the word of the <Lord /> came to Abram in a vision, saying, "Do not be afraid, Abram. I <em>am</em> your shield, your exceedingly great reward."
-        </p>
+    <p>index</p>
 
-        <p class="indent-6 text-pretty">
-          <span class="text-xs font-extralight align-text-top me-0.5">2</span>But Abram said, "Lord <God />, what will You give me, seeing I go childless, and the heir of my house <em>is</em> Eliezer of Damascus?"
-          <span class="text-xs font-extralight align-text-top me-0.5">3</span>Then Abram said, "Look, You have given me no offspring; indeed one born in my house is my heir!"
-        </p>
-
-        <p class="indent-6 text-pretty">
-          <span class="text-xs font-extralight align-text-top me-0.5">4</span>And behold, the word of the <Lord /> <em>came</em> to him, saying, "This one shall not be your heir, but one who will come from your own body shall be your heir."
-          <span class="text-xs font-extralight align-text-top me-0.5">5</span>Then He brought him outside and said, "Look now toward heaven, and count the stars if you are able to number them." And He said to him, "So shall your descendants be."
-        </p>
-
-        <p class="indent-6 text-pretty">
-          <span class="text-xs font-extralight align-text-top me-0.5">6</span>And he believed in the <Lord />, and He accounted it to him for righteousness.
-        </p>
-
-        <p class="indent-6 text-pretty">
-          <span class="text-xs font-extralight align-text-top me-0.5">7</span>Then He said to him, "I <em>am</em> the <Lord />, who brought you out of Ur of the Chaldeans, to give you this land to inherit it."
-        </p>
-
-        <p class="indent-6 text-pretty">
-          <span class="text-xs font-extralight align-text-top me-0.5">8</span>And he said, "Lord <God />, how shall I know that I will inherit it?"
-        </p>
-
-        <p class="indent-6 text-pretty">
-          <span class="text-xs font-extralight align-text-top me-0.5">9</span>So He said to him, "Bring Me a three-year-old heifer, a three-year-old female goat, a three-year-old ram, a turtledove, and a young pigeon."
-          <span class="text-xs font-extralight align-text-top me-0.5">10</span>Then he brought all these to Him and cut them in two, down the middle, and placed each piece opposite the other; but he did not cut the birds in two.
-          <span class="text-xs font-extralight align-text-top me-0.5">11</span>And when the vultures came down on the carcasses, Abram drove them away.
-        </p>
-
-        <p class="indent-6 text-pretty">
-          <span class="text-xs font-extralight align-text-top me-0.5">12</span>Now when the sun was going down, a deep sleep fell upon Abram; and behold, horror <em>and</em> great darkness fell upon him.
-          <span class="text-xs font-extralight align-text-top me-0.5">13</span>Then He said to Abram: "Know certainly that your descendants will be strangers in a land <em>that is</em> not theirs, and will serve them, and they will afflict them four hundred years.
-          <span class="text-xs font-extralight align-text-top me-0.5">14</span>And also the nation whom they serve I will judge; afterward they shall come out with great possessions.
-          <span class="text-xs font-extralight align-text-top me-0.5">15</span>Now as for you, you shall go to your fathers in peace; you shall be buried at a good old age.
-          <span class="text-xs font-extralight align-text-top me-0.5">16</span>But in the fourth generation they shall return here, for the iniquity of the Amorites <em>is</em> not yet complete."
-        </p>
-
-        <p class="indent-6 text-pretty">
-          <span class="text-xs font-extralight align-text-top me-0.5">17</span>And it came to pass, when the sun went down and it was dark, that behold, there appeared a smoking oven and a burning torch that passed between those pieces.
-          <span class="text-xs font-extralight align-text-top me-0.5">18</span>On the same day the <Lord /> made a covenant with Abram, saying:
-        </p>
-
-        <p class="indent-6 text-pretty">
-          "To your descendants I have given this land, from the river of Egypt to the great river, the River <span class="whitespace-nowrap">Euphrates&mdash;</span>
-          <span class="text-xs font-extralight align-text-top me-0.5">19</span>the Kenites, the Kenezzites, the Kadmonites,
-          <span class="text-xs font-extralight align-text-top me-0.5">20</span>the Hittites, the Perizzites, the Rephaim,
-          <span class="text-xs font-extralight align-text-top me-0.5">21</span>the Amorites, the Canaanites, the Girgashites, and the Jebusites."
-        </p>
-      </section>
-
-      <section class="my-6">
-        <h2>English Standard Version (ESV)</h2>
-        <h3 class="text-secondary font-semibold">God's Covenant with Abram</h3>
-
-        <p class="text-pretty">
-          <span class="float-left text-4xl text-secondary font-bold me-1">15</span>
-          <span class="text-xs font-extralight align-text-top me-0.5">1</span>After these things the word of the <Lord /> came to Abram in a vision: "Fear not, Abram, I am your shield; your reward shall be very great."
-          <span class="text-xs font-extralight align-text-top me-0.5">2</span>But Abram said, "O Lord <God />, what will you give me, for I continue childless, and the heir of my house is Eliezer of Damascus?"
-          <span class="text-xs font-extralight align-text-top me-0.5">3</span>And Abram said, "Behold, you have given me no offspring, and a member of my household will be my heir."
-          <span class="text-xs font-extralight align-text-top me-0.5">4</span>And behold, the word of the <Lord /> came to him: "This man shall not be your heir; your very own son shall be your heir."
-          <span class="text-xs font-extralight align-text-top me-0.5">5</span>And he brought him outside and said, "Look toward heaven, and number the stars, if you are able to number them." Then he said to him, "So shall your offspring be."
-          <span class="text-xs font-extralight align-text-top me-0.5">6</span>And he believed the <Lord />, and he counted it to him as righteousness.
-        </p>
-
-        <p class="indent-6 text-pretty">
-          <span class="text-xs font-extralight align-text-top me-0.5">7</span>And he said to him, "I am the <Lord /> who brought you out from Ur of the Chaldeans to give you this land to possess."
-          <span class="text-xs font-extralight align-text-top me-0.5">8</span>But he said, "O Lord <God />, how am I to know that I shall possess it?"
-          <span class="text-xs font-extralight align-text-top me-0.5">9</span>He said to him, "Bring me a heifer three years old, a female goat three years old, a ram three years old, a turtledove, and a young pigeon."
-          <span class="text-xs font-extralight align-text-top me-0.5">10</span>And he brought him all these, cut them in half, and laid each half over against the other. But he did not cut the birds in half.
-          <span class="text-xs font-extralight align-text-top me-0.5">11</span>And when birds of prey came down on the carcasses, Abram drove them away.
-        </p>
-
-        <p class="indent-6 text-pretty">
-          <span class="text-xs font-extralight align-text-top me-0.5">12</span>As the sun was going down, a deep sleep fell on Abram. And behold, dreadful and great darkness fell upon him.
-          <span class="text-xs font-extralight align-text-top me-0.5">13</span>Then the <Lord /> said to Abram, "Know for certain that your offspring will be sojourners in a land that is not theirs and will be servants there, and they will be afflicted for four hundred years.
-          <span class="text-xs font-extralight align-text-top me-0.5">14</span>But I will bring judgment on the nation that they serve, and afterward they shall come out with great possessions.
-          <span class="text-xs font-extralight align-text-top me-0.5">15</span>As for you, you shall go to your fathers in peace; you shall be buried in a good old age.
-          <span class="text-xs font-extralight align-text-top me-0.5">16</span>And they shall come back here in the fourth generation, for the iniquity of the Amorites is not yet complete."
-        </p>
-
-        <p class="indent-6 text-pretty">
-          <span class="text-xs font-extralight align-text-top me-0.5">17</span>When the sun had gone down and it was dark, behold, a smoking fire pot and a flaming torch passed between these pieces.
-          <span class="text-xs font-extralight align-text-top me-0.5">18</span>On that day the <Lord /> made a covenant with Abram, saying, "To your offspring I give this land, from the river of Egypt to the great river, the river Euphrates,
-          <span class="text-xs font-extralight align-text-top me-0.5">19</span>the land of the Kenites, the Kenizzites, the Kadmonites,
-          <span class="text-xs font-extralight align-text-top me-0.5">20</span>the Hittites, the Perizzites, the Rephaim,
-          <span class="text-xs font-extralight align-text-top me-0.5">21</span>the Amorites, the Canaanites, the Girgashites and the Jebusites."
-        </p>
-      </section>
-    </div>
-  </UContainer>
+    <template #footer>
+      footer
+    </template>
+  </UCard>
 </template>
